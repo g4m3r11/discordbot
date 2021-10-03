02bc930451403df6248057c097e5a0e4ee04ef7a
axolotl g4m3r <83973187+g4m3r11@users.noreply.github.com>
2021-10-02 21:23:35 -0300

    aqui
    coloque isso dentro de onde vc define a função do comando

---
+++ batatinha123.js
@@ -0,0 +1,375 @@
+var LOL = [
+"⬜\n⬜\n⬜\n⬜\n⬜\n⬜\n😎",
+"⬜\n⬜\n⬜\n⬜\n⬜\n😎\n⬜",
+"⬜\n⬜\n⬜\n⬜\n😎\n⬜\n⬜",
+"⬜\n⬜\n⬜\n😎\n⬜\n⬜\n⬜",
+"⬜\n⬜\n😎\n⬜\n⬜\n⬜\n⬜",
+"⬜\n😎\n⬜\n⬜\n⬜\n⬜\n⬜",
+"😎\n⬜\n⬜\n⬜\n⬜\n⬜\n⬜",
+"✅\n✅\n✅\n✅\n✅\n✅\n✅",
+"❌\n❌\n❌\n❌\n❌\n❌\n❌"]
+
+var LOL2 = [
+"⬜\n⬜\n⬜\n⬜\n⬜\n⬜\n😐",
+"⬜\n⬜\n⬜\n⬜\n⬜\n😐\n⬜",
+"⬜\n⬜\n⬜\n⬜\n😐\n⬜\n⬜",
+"⬜\n⬜\n⬜\n😐\n⬜\n⬜\n⬜",
+"⬜\n⬜\n😐\n⬜\n⬜\n⬜\n⬜",
+"⬜\n😐\n⬜\n⬜\n⬜\n⬜\n⬜",
+"😐\n⬜\n⬜\n⬜\n⬜\n⬜\n⬜",
+"✅\n✅\n✅\n✅\n✅\n✅\n✅",
+"❌\n❌\n❌\n❌\n❌\n❌\n❌"]
+
+var LOL3 = [
+"⬜\n⬜\n⬜\n⬜\n⬜\n⬜\n🙄",
+"⬜\n⬜\n⬜\n⬜\n⬜\n🙄\n⬜",
+"⬜\n⬜\n⬜\n⬜\n🙄\n⬜\n⬜",
+"⬜\n⬜\n⬜\n🙄\n⬜\n⬜\n⬜",
+"⬜\n⬜\n🙄\n⬜\n⬜\n⬜\n⬜",
+"⬜\n🙄\n⬜\n⬜\n⬜\n⬜\n⬜",
+"🙄\n⬜\n⬜\n⬜\n⬜\n⬜\n⬜",
+"✅\n✅\n✅\n✅\n✅\n✅\n✅",
+"❌\n❌\n❌\n❌\n❌\n❌\n❌"]
+
+var LOL4 = [
+"⬜\n⬜\n⬜\n⬜\n⬜\n⬜\n😨",
+"⬜\n⬜\n⬜\n⬜\n⬜\n😨\n⬜",
+"⬜\n⬜\n⬜\n⬜\n😨\n⬜\n⬜",
+"⬜\n⬜\n⬜\n😨\n⬜\n⬜\n⬜",
+"⬜\n⬜\n😨\n⬜\n⬜\n⬜\n⬜",
+"⬜\n😨\n⬜\n⬜\n⬜\n⬜\n⬜",
+"😨\n⬜\n⬜\n⬜\n⬜\n⬜\n⬜",
+"✅\n✅\n✅\n✅\n✅\n✅\n✅",
+"❌\n❌\n❌\n❌\n❌\n❌\n❌"]
+
+var LOL5 = [
+"⬜\n⬜\n⬜\n⬜\n⬜\n⬜\n🙂",
+"⬜\n⬜\n⬜\n⬜\n⬜\n🙂\n⬜",
+"⬜\n⬜\n⬜\n⬜\n🙂\n⬜\n⬜",
+"⬜\n⬜\n⬜\n🙂\n⬜\n⬜\n⬜",
+"⬜\n⬜\n🙂\n⬜\n⬜\n⬜\n⬜",
+"⬜\n🙂\n⬜\n⬜\n⬜\n⬜\n⬜",
+"🙂\n⬜\n⬜\n⬜\n⬜\n⬜\n⬜",
+"✅\n✅\n✅\n✅\n✅\n✅\n✅",
+"❌\n❌\n❌\n❌\n❌\n❌\n❌"]
+
+var LOL6 = [
+"⬜\n⬜\n⬜\n⬜\n⬜\n⬜\n🤔",
+"⬜\n⬜\n⬜\n⬜\n⬜\n🤔\n⬜",
+"⬜\n⬜\n⬜\n⬜\n🤔\n⬜\n⬜",
+"⬜\n⬜\n⬜\n🤔\n⬜\n⬜\n⬜",
+"⬜\n⬜\n🤔\n⬜\n⬜\n⬜\n⬜",
+"⬜\n🤔\n⬜\n⬜\n⬜\n⬜\n⬜",
+"🤔\n⬜\n⬜\n⬜\n⬜\n⬜\n⬜",
+"✅\n✅\n✅\n✅\n✅\n✅\n✅",
+"❌\n❌\n❌\n❌\n❌\n❌\n❌"]
+
+var tempo = 0;
+var j1 = 0; 
+var j2 = 0;
+var j3 = 0;
+var j4 = 0;
+var j5 = 0;
+var j6 = 0;
+
+var luzUwU = "🟢";
+var luzDIE = "🔴"
+
+var lol = [
+  {luz: "🔴", cor: 'RED'},
+  {luz: "🟢", cor: 'GREEN'}
+]
+var random = lol[Math.floor(Math.random() * lol.length)]
+
+const Embed = new Discord.MessageEmbed()
+        .setDescription(":one:")
+const newEmbed = new Discord.MessageEmbed()
+        .setDescription(":two:")
+const newnewEmbed = new Discord.MessageEmbed()
+		.setDescription(":three:")
+const game = new Discord.MessageEmbed() 
+		.setTitle("Batatinha123...                       <:ead:893996941128663110>")
+		.setDescription("🅾👩🏻🅾")
+		.addFields(
+		{ name: '🟧', value: LOL[j1], inline: true },
+		{ name: '🟧', value: LOL2[j2], inline: true },
+		{ name: '🟧', value: LOL3[j3], inline: true },
+		{ name: '🟧', value: LOL4[j4], inline: true },
+		{ name: '🟧', value: LOL5[j5], inline: true },
+		{ name: '🟧', value: LOL6[j6], inline: true },
+		)
+		.setColor('GREEN')
+		.setFooter(luzUwU);
+
+var Msg = await message.channel.send(Embed);
+
+	setTimeout(() => {
+		Msg.edit(newEmbed)
+	}, 1000);
+
+	setTimeout(() => {
+		Msg.edit(newnewEmbed)
+	}, 2000);
+
+	setTimeout(() => {
+		Msg.edit(game);
+		Msg.react("😎");
+		Msg.react('😐');
+		Msg.react('🙄');
+		Msg.react('😨');
+		Msg.react('🙂');
+		Msg.react('🤔');
+	}, 3000);
+
+var gameCMC = new Discord.MessageEmbed()
+		.setTitle("Batatinha123...                       <:ead:893996941128663110>")
+		.setDescription("🅾👩🏻🅾" + tempo)
+		.addFields(
+		{ name: '🟧', value: LOL[j1], inline: true },
+		{ name: '🟧', value: LOL2[j2], inline: true },
+		{ name: '🟧', value: LOL3[j3], inline: true },
+		{ name: '🟧', value: LOL4[j4], inline: true },
+		{ name: '🟧', value: LOL5[j5], inline: true },
+		{ name: '🟧', value: LOL6[j6], inline: true },
+		)
+		.setFooter(random.luz)
+		.setColor(random.cor);
+
+interval = setInterval(() => {
+	gameCMC.setDescription("🅾👩🏻🅾\nTempo:" + tempo + " / 50")
+    random = lol[Math.floor(Math.random() * lol.length)];
+	gameCMC.setFooter(random.luz)
+	gameCMC.setColor(random.cor)		
+	Msg.edit(gameCMC);
+	tempo++;
+	if (tempo == 51) {message.channel.send("ACABOU O TEMPO, jogadores que não passaram foram eliminados");
+	gameCMC.setFooter(luzDIE)
+	gameCMC.setColor('RED')
+	clearInterval(interval);}
+}, 5000); 
+
+
+var filter = (reaction, user) => {
+    return reaction.emoji.name == ('😎') && user.id != '836662889217261628'
+};
+                
+        var collector = Msg.createReactionCollector(filter, { time: 300000 })
+                
+        collector.on('collect', (reaction, user) => {
+				Msg.reactions.cache.get('😎').remove()
+				if(Msg.embeds[0].footer.text.includes("🟢") && (j1 + 1) < 7 && tempo < 51) {
+					Msg.react("😎");
+            		j1++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[0] = { name: '1️⃣', value: LOL[j1], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)		
+					Msg.edit(gameCMC);
+				} else if (Msg.embeds[0].footer.text.includes("🔴")) {
+					Msg.react("❌");
+					j1 = 8;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[0] = { name: '❎', value: LOL[j1], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)
+					/*Msg.edit(gameCMC2);*/		
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 1️⃣ eliminado");
+				} else if (Msg.embeds[0].footer.text.includes("🟢") && (j1 + 1) == 7) {
+					Msg.react("🏅");
+            		j1++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[0] = { name: '1️⃣', value: LOL[j1], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 1️⃣ passou");
+				}
+			})
+        
+
+var filter2 = (reaction, user) => {
+    return reaction.emoji.name == ('😐') && user.id != '836662889217261628'
+};
+                
+        var collector2 = Msg.createReactionCollector(filter2, { time: 300000 })
+                
+        collector2.on('collect', (reaction, user) => {
+				Msg.reactions.cache.get('😐').remove()
+				if(Msg.embeds[0].footer.text.includes("🟢") && (j2 + 1) < 7 && tempo < 51) {
+					Msg.react("😐");
+            		j2++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[1] = { name: '2️⃣', value: LOL2[j2], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)
+					/*Msg.edit(gameCMC2);*/		
+					Msg.edit(gameCMC);
+				} else if (Msg.embeds[0].footer.text.includes("🔴")) {
+					Msg.react("❌");
+					j2 = 8;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[1] = { name: '❎', value: LOL2[j2], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)
+					/*Msg.edit(gameCMC2);*/		
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 2️⃣ eliminado");
+				} else if (Msg.embeds[0].footer.text.includes("🟢") && (j2 + 1) == 7) {
+					Msg.react("🏅");
+            		j2++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[1] = { name: '2️⃣', value: LOL2[j2], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)
+					/*Msg.edit(gameCMC2);*/		
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 2️⃣ passou");
+				}
+			})
+
+var filter3 = (reaction, user) => {
+    return reaction.emoji.name == ('🙄') && user.id != '836662889217261628'
+};
+                
+        var collector3 = Msg.createReactionCollector(filter3, { time: 300000 })
+                
+        collector3.on('collect', (reaction, user) => {
+				Msg.reactions.cache.get('🙄').remove()
+				if(Msg.embeds[0].footer.text.includes("🟢") && (j3 + 1) < 7 && tempo < 51) {
+					Msg.react("🙄");
+            		j3++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[2] = { name: '3️⃣', value: LOL3[j3], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+				} else if (Msg.embeds[0].footer.text.includes("🔴")) {
+					Msg.react("❌");
+					j3 = 8;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[2] = { name: '❎', value: LOL3[j3], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)		
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 3️⃣ eliminado");
+				} else if (Msg.embeds[0].footer.text.includes("🟢") && (j3 + 1) == 7) {
+					Msg.react("🏅");
+            		j3++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[2] = { name: '3️⃣', value: LOL3[j3], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 3️⃣ passou");
+				}
+			})
+var filter4 = (reaction, user) => {
+    return reaction.emoji.name == ('😨') && user.id != '836662889217261628'
+};
+                
+        var collector4 = Msg.createReactionCollector(filter4, { time: 300000 })
+                
+        collector4.on('collect', (reaction, user) => {
+				Msg.reactions.cache.get('😨').remove()
+				if(Msg.embeds[0].footer.text.includes("🟢") && (j4 + 1) < 7 && tempo < 51) {
+					Msg.react("😨");
+            		j4++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[3] = { name: '4️⃣', value: LOL4[j4], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+				} else if (Msg.embeds[0].footer.text.includes("🔴")) {
+					Msg.react("❌");
+					j4 = 8;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[3] = { name: '❎', value: LOL4[j4], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)		
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 4️⃣ eliminado");
+				} else if (Msg.embeds[0].footer.text.includes("🟢") && (j4 + 1) == 7) {
+					Msg.react("🏅");
+            		j4++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[3] = { name: '4️⃣', value: LOL4[j4], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 4️⃣ passou");
+				}
+			})
+
+var filter5 = (reaction, user) => {
+    return reaction.emoji.name == ('🙂') && user.id != '836662889217261628'
+};
+                
+        var collector5 = Msg.createReactionCollector(filter5, { time: 300000 })
+                
+        collector5.on('collect', (reaction, user) => {
+				Msg.reactions.cache.get('🙂').remove()
+				if(Msg.embeds[0].footer.text.includes("🟢") && (j5 + 1) < 7 && tempo < 51) {
+					Msg.react("🙂");
+            		j5++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[4] = { name: '5️⃣', value: LOL5[j5], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+				} else if (Msg.embeds[0].footer.text.includes("🔴")) {
+					Msg.react("❌");
+					j5 = 8;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[4] = { name: '❎', value: LOL5[j5], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)		
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 5️⃣ eliminado");
+				} else if (Msg.embeds[0].footer.text.includes("🟢") && (j5 + 1) == 7) {
+					Msg.react("🏅");
+            		j5++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[4] = { name: '5️⃣', value: LOL5[j5], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 5️⃣ passou");
+				}
+			})
+var filter6 = (reaction, user) => {
+    return reaction.emoji.name == ('🤔') && user.id != '836662889217261628'
+};
+                
+        var collector6 = Msg.createReactionCollector(filter6, { time: 300000 })
+                
+        collector6.on('collect', (reaction, user) => {
+				Msg.reactions.cache.get('🤔').remove()
+				if(Msg.embeds[0].footer.text.includes("🟢") && (j6 + 1) < 7 && tempo < 51) {
+					Msg.react("🤔");
+            		j6++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[5] = { name: '6️⃣', value: LOL6[j6], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+				} else if (Msg.embeds[0].footer.text.includes("🔴")) {
+					Msg.react("❌");
+					j6 = 8;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[5] = { name: '❎', value: LOL6[j6], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)		
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 6️⃣ eliminado");
+				} else if (Msg.embeds[0].footer.text.includes("🟢") && (j6 + 1) == 7) {
+					Msg.react("🏅");
+            		j6++;
+					random = lol[Math.floor(Math.random() * lol.length)];
+					gameCMC.fields[5] = { name: '6️⃣', value: LOL6[j6], inline: true }
+					gameCMC.setFooter(random.luz)
+					gameCMC.setColor(random.cor)	
+					Msg.edit(gameCMC);
+					message.channel.send("Jogador 6️⃣ passou");
+				}
+			})
